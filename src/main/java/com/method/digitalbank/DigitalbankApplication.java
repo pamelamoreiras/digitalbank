13760d43a139c0a80bec3a241d8fcd1ab40c85c4
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-09-16 14:44:47 -0300

    [DIGITAL-BANK] setup inicial do projeto digital bank.

---
+++ src/main/java/com/method/digitalbank/DigitalbankApplication.java
@@ -0,0 +1,13 @@
+package com.method.digitalbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalbankApplication.class, args);
+	}
+
+}
